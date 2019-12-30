e0ee90e90e7343d2482f72915d8dd571ca14c702
Mitja Karhusaari <mitja@karhusaari.me>
2019-12-30 19:20:25 +0200

    Separate deployment into a script

---
+++ publish.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# This script is called by the Travis CI pipeline when a new tag has been created.
+# wasm-pack creates a ready-to-publish NPM package into a directory called pkg.
+
+cd "$TRAVIS_BUILD_DIR/pkg"
+npm publish
